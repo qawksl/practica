d6ec4a2d46fbd6327f885d682fd8cc2a146fea75
H8YOU212 <slavyan.212@mail.ru>
2024-11-20 11:58:20 +0500

    выводиться спиок поциента

--- database.py
+++ database.py
@@ -1,9 +1,33 @@
 import sqlalchemy as sqla
 import database
 
-CONNECTION_STRING = "mysql+pymysql://root@192.168.3.111/is61-3/agt2ge8r"
+CONNECTION_STRING = "mysql+pymysql://is61-3:agt2ge8r@192.168.3.111/aaa"
 
 class Database():
     def __init__(self):
         self.engine = sqla.create_engine(CONNECTION_STRING)
         self.connaction = self.engine.connect()
+
+    def translate_to_dict(self, result_raw):
+        result = []
+        for r in result_raw:
+            result.append(r._asdict())    
+        return result
+    
+    def get_patients(self):
+        query = sqla.text("SELECT * FROM patients")
+        result_raw = self.connaction.execute(query).all()
+        return self.translate_to_dict(result_raw)
+    
+    def del_patients(self,id):
+        query = sqla.text("DELETE FROM patients WHERE id = :id")
+        query = query.bindparams(sqla.bindparam("id", id))
+        self.connaction.execute(query)
+        self.connaction.commit()        
+
+   
+  
+    
+if __name__ == "__main__":
+    db = Database()
+    print(db.get_patients())
--- main_window.py
+++ main_window.py
@@ -10,3 +10,57 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Главная")
         self.resize(800,600)
+
+        list_model = QStringListModel()
+        list_model.setStringList(self.get_patients())
+
+        list_widget = QWidget()
+        self.list_view = QListView(list_widget)
+        self.list_view.setModel(list_model)
+        self.list_view.resize(800,600)
+
+        self.add_button = QPushButton("Добавить")
+        self.del_button = QPushButton("Удалить")
+        self.edit_button = QPushButton("Изменить")
+        self.update_button = QPushButton("Обновить")
+        self.update_button.clicked.connect(self.update_list_view_patients)
+        self.otdel_button = QPushButton("Отделение")
+
+
+        buttons = QHBoxLayout()
+        buttons.addWidget(self.add_button)
+        buttons.addWidget(self.del_button)
+        buttons.addWidget(self.edit_button)
+        buttons.addWidget(self.update_button)
+        buttons.addWidget(self.otdel_button)
+        buttons_widget = QWidget()
+        buttons_widget.setLayout(buttons)
+
+        layout = QVBoxLayout()
+        layout.addWidget(buttons_widget)
+        layout.addWidget(list_widget)
+        widget = QWidget()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(widget)
+
+    def get_patients(self):
+        db = Database()
+        result_raw = db.get_patients()
+        result = []
+        for r in result_raw:
+            result.append(str(r["id"])+ "."+ r["name"]+ " "+ r["firstname"]+ " "+ r["otchestvo"] )
+        return result
+
+    def get_list_model_patients(self):
+        list_model = QStringListModel()
+        list_model.setStringList(self.get_patients())
+        return list_model
+    
+    def update_list_view_patients(self):
+        self.list_view.setModel(self.get_list_model_patients())
+
+    
+
+    
+
